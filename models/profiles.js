0ee1a063cf47ef17dc8e59501937a4d710b39ef4
Loh Chun Mun <chunmun_loh@live.com>
2022-08-18 02:36:52 +0800

    create profiles model

---
+++ models/profiles.js
@@ -0,0 +1,29 @@
+const db = require('./db');
+
+// CREATE TABLE "profiles" (
+// 	"id"	INTEGER NOT NULL,
+// 	"code"	TEXT NOT NULL UNIQUE,
+// 	"firstName"	TEXT NOT NULL,
+// 	"lastName"	TEXT NOT NULL,
+// 	"nric"	TEXT NOT NULL,
+// 	"organisation"	TEXT,
+// 	PRIMARY KEY("id" AUTOINCREMENT)
+// );
+
+function createProfile(profile) {
+    return db('profiles').insert(profile);
+}
+
+function getProfile(code) {
+    return db('profiles').select('*').where('code',  code);
+}
+
+function updateProfile(code, profile) {
+    return db('profiles').where('code', code).update(profile);
+}
+
+module.exports = {
+    createProfile,
+    getProfile,
+    updateProfile
+}
