84105a110fa40af6ef23c7e810eed2d17be3ac25
Loh Chun Mun <chunmun_loh@live.com>
2022-08-18 17:50:10 +0800

    added logger, bootstrap & jquery

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-
-const apiRoutes = require('./routes/api');
+const logger = require('morgan');
 
 const errorController = require('./controllers/error');
+
+const frontendRoutes = require('./routes/frontend');
+const apiRoutes = require('./routes/api');
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,14 +18,24 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// serve bootstrap & jquery
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
+// middleware
 app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false})); // parse application/x-www-form-urlencoded
+app.use(logger('dev'));
 
 // allow the server to accept requests from external clients for api endpoints
 const corsOptions = {
     methods: "GET, POST, PUT, DELETE, OPTIONS"
 }
 app.use(cors(corsOptions));
+
+// frontend route
+app.use('/', frontendRoutes);
 
 // api endpoint route
 app.use('/api', apiRoutes);
