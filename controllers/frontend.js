ab2e00dfaddf9ed8eb3f5f46711e29071ba7fd00
Loh Chun Mun <chunmun_loh@live.com>
2022-08-18 22:02:17 +0800

    added verify profile page

--- controllers/frontend.js
+++ controllers/frontend.js
@@ -2,6 +2,12 @@ const request = require('request');
 const HTTP_STATUS = require("../constants/http_status");
 
 exports.viewProfile = async (req, res, next) => {
+    // if query code is empty
+    if (!req.query.code) {
+        res.status(HTTP_STATUS.NOT_FOUND).redirect('/verifyProfile?result=false');
+        return;
+    }
+
     const host = req.protocol + '://' + req.hostname + ':' + process.env.PORT;
     const endpoint = '/api/getProfile/';
     request(host + endpoint + req.query.code, (error, response, body) => {
@@ -16,15 +22,19 @@ exports.viewProfile = async (req, res, next) => {
                 title: result.gender === "Male" ? "Mr." : "Ms."
             }
             res.status(HTTP_STATUS.OK).render('profile', profile);
-        }
-        else
-            res.status(HTTP_STATUS.NOT_FOUND).render('error', {
-                statusCode: HTTP_STATUS.NOT_FOUND,
-                message: 'User not found'
-            });
+        } else
+            res.status(HTTP_STATUS.NOT_FOUND).redirect('/verifyProfile?result=false&search=' + req.query.code);
     });
 };
 
 exports.verifyProfile = async (req, res, next) => {
-    res.status(HTTP_STATUS.OK).render('verifyProfile');
+    if (!req.query.search && !req.query.result) {
+        res.status(HTTP_STATUS.OK).render('verifyProfile');
+    }
+    else {
+        res.status(HTTP_STATUS.NOT_FOUND).render('verifyProfile', {
+            search: req.query.search,
+            message: 'The identification code is invalid.'
+        });
+    }
 };
