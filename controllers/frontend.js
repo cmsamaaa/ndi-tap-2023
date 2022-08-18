191fc2778bdeb1da2b0d223ab502c152262a4f4a
Loh Chun Mun <chunmun_loh@live.com>
2022-08-19 00:53:40 +0800

    added index page

--- controllers/frontend.js
+++ controllers/frontend.js
@@ -1,6 +1,10 @@
 const request = require('request');
 const HTTP_STATUS = require("../constants/http_status");
 
+exports.viewIndex = async (req, res, next) => {
+    res.status(HTTP_STATUS.OK).render('index');
+};
+
 exports.viewProfile = async (req, res, next) => {
     // if query code is empty
     if (!req.query.code) {
--- routes/frontend.js
+++ routes/frontend.js
@@ -4,6 +4,7 @@ const frontendController = require('../controllers/frontend');
 
 const router = express.Router();
 
+router.get('/', frontendController.viewIndex);
 router.get('/viewProfile', frontendController.viewProfile);
 router.get('/verifyProfile', frontendController.verifyProfile);
 
