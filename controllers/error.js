d11ca833a6528f50f444b0afb85a5bd6631b277e
Loh Chun Mun <chunmun_loh@live.com>
2022-08-20 03:59:14 +0800

    remove unused attribute from response body for error page

--- controllers/error.js
+++ controllers/error.js
@@ -3,7 +3,6 @@ const HTTP_STATUS = require("../constants/http_status");
 exports.get404 = (req, res, next) => {
     res.status(HTTP_STATUS.NOT_FOUND).render('error', {
         statusCode: HTTP_STATUS.NOT_FOUND,
-        result: null,
         message: 'Page not found'
     });
 };
