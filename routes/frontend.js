14c60e3e1d44ceac37afb06b8a8959f018121903
Loh Chun Mun <chunmun_loh@live.com>
2022-08-20 00:47:50 +0800

    added index path

--- routes/frontend.js
+++ routes/frontend.js
@@ -5,6 +5,7 @@ const frontendController = require('../controllers/frontend');
 const router = express.Router();
 
 router.get('/', frontendController.viewIndex);
+router.get('/index', frontendController.viewIndex);
 router.get('/viewProfile', frontendController.viewProfile);
 router.get('/verify', frontendController.verify);
 router.get('/register', frontendController.register);
